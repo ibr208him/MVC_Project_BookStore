449b078a0d2bb25e45b119835d2fe91b2aed14aa
Ibrahim Maali <maaliibrahim2022@gmail.com>
2024-05-18 04:14:55 +0300

    show categories

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project_BookStore.Data;
+
+namespace MVC_Project_BookStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.categories.ToList();
+            return View(result);
+        }
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MVC_Project_BookStore.Models;
 
 namespace MVC_Project_BookStore.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Category> categories { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
